96545541bd3a53c2704b3d8cd63e5a08491fcc7e
penghai <564990676@qq.com>
2020-12-20 16:33:38 +0800

    解决冲突了

--- axios/src/test/java/it/ph/springboot/vue/axios/service/ClassServiceImplTest.java
+++ axios/src/test/java/it/ph/springboot/vue/axios/service/ClassServiceImplTest.java
@@ -19,16 +19,29 @@ class ClassServiceImplTest {
     }
 
     @Test
-<<<<<<< HEAD
+
     void deledaeClass() {
         classServiceImpl.deledaeClass(1);
         //我也修改了代码
     }
-=======
+
     void updetaClass() {
         ClassModel classModel = new ClassModel();
         classServiceImpl.updetaClass(classModel);
     }
     //我经行了修改了
->>>>>>> f042ff4a1fe435556e0050113403b1efef96060f
+//改解决代码冲突了
+    /**
+     * fg
+     * gr
+     * g
+     * rg
+     * rh
+     * t
+     * ut
+     * y
+     * t
+     * y
+     *
+     */
 }
