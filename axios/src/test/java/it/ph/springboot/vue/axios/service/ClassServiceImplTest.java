f042ff4a1fe435556e0050113403b1efef96060f
penghai <564990676@qq.com>
2020-12-20 16:28:36 +0800

    git中修改了代码

--- axios/src/test/java/it/ph/springboot/vue/axios/service/ClassServiceImplTest.java
+++ axios/src/test/java/it/ph/springboot/vue/axios/service/ClassServiceImplTest.java
@@ -1,6 +1,7 @@
 package it.ph.springboot.vue.axios.service;
 
 import it.ph.springboot.vue.axios.AxiosApplication;
+import it.ph.springboot.vue.axios.model.ClassModel;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,4 +17,11 @@ class ClassServiceImplTest {
     void queryModel() {
         classServiceImpl.queryModel();
     }
+
+    @Test
+    void updetaClass() {
+        ClassModel classModel = new ClassModel();
+        classServiceImpl.updetaClass(classModel);
+    }
+    //我经行了修改了
 }
