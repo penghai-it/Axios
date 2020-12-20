2cdd8235d2c22464a7ea6f2bf9a49d74b4ce5ed9
penghai <564990676@qq.com>
2020-12-20 16:51:06 +0800

    修改了api类

--- axios/src/main/java/it/ph/springboot/vue/axios/api/Api.java
+++ axios/src/main/java/it/ph/springboot/vue/axios/api/Api.java
@@ -1,4 +1,14 @@
 package it.ph.springboot.vue.axios.api;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Api {
+    private int id;
+    private String name;
+    private int aeg;
 }
